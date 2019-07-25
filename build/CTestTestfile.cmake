5f9ae93b46cfbcb9228aca02c1ceccf84c95f9e4
Jidong Chen <jidong@JidongdeMacBook-Pro.local>
2019-07-25 10:55:40 +0800

    rm build dir from repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jidong/Desktop/Documents/projects/cpp_projects/simple_compiler
-# Build directory: /Users/jidong/Desktop/Documents/projects/cpp_projects/simple_compiler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
